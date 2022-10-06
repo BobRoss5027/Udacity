57ea2d55665d2ea6a87579b528e08b9b9ee28d0a
Eoin McNeice <eoin@astraappivate.co.uk>
2022-10-06 09:36:23 +0100

    bug fixing

--- AzureDevOps/WebServerProject/main.tf
+++ AzureDevOps/WebServerProject/main.tf
@@ -21,10 +21,10 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_security_group" "example" {
+resource "azurerm_network_security_group" "main" {
   name                = "Basic Communication Control"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   security_rule {
     name                       = "Intranet"
@@ -55,19 +55,19 @@ resource "azurerm_public_ip" "external" {
   location = azurerm_resource_group.main.location
   allocation_method = "Static"
 }
-resource "azurerm_lb" "example" {
+resource "azurerm_lb" "main" {
   name                = "LoadBalancer"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
+    public_ip_address_id = azurerm_public_ip.main.id
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "example" {
-  loadbalancer_id = azurerm_lb.example.id
+resource "azurerm_lb_backend_address_pool" "main" {
+  loadbalancer_id = azurerm_lb.main.id
   name            = "BackEndAddressPool"
 }
 
@@ -83,19 +83,19 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_managed_disk" "example" {
+resource "azurerm_managed_disk" "main" {
   name                 = "acctestmd"
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
+  location             = azurerm_resource_group.main.location
+  resource_group_name  = azurerm_resource_group.main.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = "1"
 }
 
-resource "azurerm_availability_set" "example" {
-  name                = "example-aset"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_availability_set" "main" {
+  name                = "main-aset"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
