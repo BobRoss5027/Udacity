dc52dee01da6a5dfdbab681d3257f2b0e657ce36
Eoin McNeice <eoin@astraappivate.co.uk>
2022-10-07 14:22:45 +0100

    Changed the resource names properly

--- AzureDevOps/WebServerProject/main.tf
+++ AzureDevOps/WebServerProject/main.tf
@@ -10,21 +10,21 @@ resource "azurerm_resource_group" "main_rg" {
 resource "azurerm_virtual_network" "main_vn" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/22"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
 }
 
 resource "azurerm_subnet" "main_sub" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.main_rg.name
+  virtual_network_name = azurerm_virtual_network.main_vn.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_security_group" "main_nsg" {
   name                = "BasicCommunicationControl"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
 
   security_rule {
     name                       = "Intranet"
@@ -53,43 +53,43 @@ resource "azurerm_network_security_group" "main_nsg" {
 
 resource "azurerm_public_ip" "main_pubip" {
   name = "external"
-  resource_group_name = azurerm_resource_group.main.name
-  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  location = azurerm_resource_group.main_rg.location
   allocation_method = "Static"
 }
 resource "azurerm_lb" "main_lb" {
   name                = "LoadBalancer"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.external.id
+    public_ip_address_id = azurerm_public_ip.main_pubip.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "main_pool" {
-  loadbalancer_id = azurerm_lb.main.id
+  loadbalancer_id = azurerm_lb.main_lb.id
   name            = "BackEndAddressPool"
 }
 
 resource "azurerm_network_interface" "main_nic" {
   count="${var.amount}"
   name                = "${var.prefix}${count.index}-nic"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  location            = azurerm_resource_group.main_rg.location
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = azurerm_subnet.main_sub.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_managed_disk" "main_disk" {
   name                 = "acctestmd"
-  location             = azurerm_resource_group.main.location
-  resource_group_name  = azurerm_resource_group.main.name
+  location             = azurerm_resource_group.main_rg.location
+  resource_group_name  = azurerm_resource_group.main_rg.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = "1"
@@ -97,23 +97,23 @@ resource "azurerm_managed_disk" "main_disk" {
 
 resource "azurerm_availability_set" "main_aset" {
   name                = "main-aset"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
   platform_fault_domain_count = 2
 }
 
 resource "azurerm_linux_virtual_machine" "main_vm" {
   count                           = "${var.amount}"
   name                            = "${var.prefix}${count.index}-vm"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
+  resource_group_name             = azurerm_resource_group.main_rg.name
+  location                        = azurerm_resource_group.main_rg.location
   size                            = "Standard_D2s_v3"
   admin_username                  = "${var.username}"
   admin_password                  = "${var.password}"
-  availability_set_id             = azurerm_availability_set.main.id
+  availability_set_id             = azurerm_availability_set.main_aset.id
   disable_password_authentication = false
   network_interface_ids = [element(
-    azurerm_network_interface.main.id,count.index
+    azurerm_network_interface.main_nic.id,count.index
   )]
 
   source_image_reference {
