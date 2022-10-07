0af2b5e455d2a1fc4b7d3aa525c098d509dc3eb7
Eoin McNeice <eoin@astraappivate.co.uk>
2022-10-07 14:38:31 +0100

    Added default values to variables.tf

--- AzureDevOps/WebServerProject/variables.tf
+++ AzureDevOps/WebServerProject/variables.tf
@@ -1,19 +1,24 @@
 variable "prefix" {
+  default = "test"
   description = "The prefix which should be used for all resources in this example"
 }
 
 variable "location" {
+  default = "uksouth"
   description = "The Azure Region in which all resources in this example should be created."
 }
 
 variable "username" {
+  default = "testAccount"
   description = "The username for the virtual machine"
 }
 
 variable "password" {
+  default = "P@ssw0rd"
   description = "The password for the virtual machine"
 }
 
 variable "amount" {
+  default = "2"
   description = "The amount of virtual machines to create"
 }
